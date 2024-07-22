6394773b4dfbb43bac8c23cc15e88666cc101056
Seif-Gama1 <126584186+Seif-Gama1@users.noreply.github.com>
2024-07-22 14:14:14 +0300

    Add files via upload

--- Assignment9/js/main.js
+++ Assignment9/js/main.js
@@ -19,14 +19,36 @@ var searchInput = document.querySelector("#search-input");
 const baseUrl = 'https://api.themoviedb.org/3';
 const getMovies = '/movie/now_playing';
 
+var submitbtn = document.querySelector("#submitid");
+
 var allMoviesContainer = document.querySelector("#movie-container");
 
+
+//Menu Buttons
 var apiButton1 = document.querySelector("#m-btn-1");
 var apiButton2 = document.querySelector("#m-btn-2");
 var apiButton3 = document.querySelector("#m-btn-3");
 var apiButton4 = document.querySelector("#m-btn-4");
 var apiButton5 = document.querySelector("#m-btn-5");
 
+//Contact us inputs
+var nameBox = document.querySelector("#name-box");
+var emailBox = document.querySelector("#email-box");
+var phoneBox = document.querySelector("#phone-box");
+var ageBox = document.querySelector("#age-box");
+var passBox = document.querySelector("#password-box");
+var validatePassBox = document.querySelector("#validate-pass-box");
+
+//contact us errors
+var nameError = document.querySelector("#name-error");
+var emailError = document.querySelector("#email-error");
+var phoneError = document.querySelector("#phone-error");
+var ageError = document.querySelector("#age-error");
+var passwordError = document.querySelector("#password-error");
+var validatePassError = document.querySelector("#validate-pass-error");
+
+let nameProhibitedchars =['1','2','3','4','5','6','7','8','9','0','%','#','@','!','&','$','*','+','-','=','/','<','>','?'];
+let emailallowedservices=["@gmail.com","@yahoo.com","@hotmail.com"] ;
 
 getApi(nowPlayingMoviesURL);
 
@@ -132,9 +154,75 @@ searchInput.addEventListener('input', () => {
         const searchUrl = `${baseUrl}/search/movie?api_key=${API_Key}&query=${query}`;
         mainTitle.innerHTML = `Results for "${query}"`;
         getApi(searchUrl);
-    }
-    else {
+    }else {
         mainTitle.innerHTML = `New playing`;
         getApi(`${baseUrl}${getMovies}?api_key=${API_Key}`);
+    }
+});
+
+submitbtn.addEventListener("click" ,function(){
+    let nameflag=0;
+    let eflag=0;
+    nameProhibitedchars.forEach(subString => {
+        if (nameBox.value.includes(subString)) {
+            nameflag=1;
+        } 
+    });
+    if(nameflag){
+        nameError.innerText="It contains prohibited characters";
+        nameBox.style.border="red 1px solid";
+    }else{
+        nameError.innerText="";
+        nameBox.style.border="none";
+    }
+
+    emailallowedservices.forEach(sub=>{
+        if(emailBox.value.includes(sub)){
+            eflag=1;
+        }
+    });
+    if(!eflag){
+        emailError.innerText="Invalid Email" ;
+        emailBox.style.border="red 1px solid";
+    }else{
+        emailError.innerText="";
+        emailBox.style.border="none";
+    }
+
+
+    if(phoneBox.value.length!=11){
+        phoneError.innerText="Invalid Number, It must be 11 digits";
+        phoneBox.style.border="red 1px solid";
+    }else{
+        phoneError.innerText="";
+        phoneBox.style.border="none";
+    }
+
+
+    if(!(Number(ageBox.value)>0 &&Number(ageBox.value)<110  )){
+        ageError.innerText="Invalid Age" ;
+        ageBox.style.border="red 1px solid";
+    }else{
+        ageError.innerText="" ;
+        ageBox.style.border="none";
+    }
+
+
+    if(!(passBox.value.length>10)){
+        passwordError.innerText="Easy password  it must be larger than 10 characters";
+        passBox.style.border="red 1px solid";
+    }else{
+        passwordError.innerText="";
+        passBox.style.border="none";
+    }
+
+
+    if(passBox.value!==validatePassBox.value){
+        validatePassError.innerText="Passwords are not matching" ;
+        validatePassBox.style.border="red 1px solid";
+
+    }else{
+        validatePassError.innerText="" ;
+        validatePassBox.style.border="none";
     }
 });
